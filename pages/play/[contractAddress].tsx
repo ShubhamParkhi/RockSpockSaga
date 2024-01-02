bfdb6c00677c0420363dfa4a9b2637eccad7d120
Shubham Parkhi <shubhamparkhi10@gmail.com>
2024-01-03 04:08:11 +0530

    Update [contractAddress].tsx

--- pages/play/[contractAddress].tsx
+++ pages/play/[contractAddress].tsx
@@ -1,11 +1,13 @@
-import { NextPage } from 'next';
-import React from 'react';
-import WaitingForOpponentsMove from '../../components/Waiting for Opponents Move/WaitingForOpponentsMove';
+import { NextPage } from "next";
+import React from "react";
+import WaitingForOpponentsMove from "../../components/Waiting for Opponents Move/WaitingForOpponentsMove";
 
-const WaitingForOpponent: NextPage<{ contractAddress: string }> = ({ contractAddress }) => {
-  return (
-      <WaitingForOpponentsMove contractAddress={contractAddress} />
-  );
+const WaitingForOpponent: NextPage<{ contractAddress: string }> = ({
+  contractAddress,
+}: {
+  contractAddress: string;
+}) => {
+  return <WaitingForOpponentsMove contractAddress={contractAddress} />;
 };
 
 export default WaitingForOpponent;
