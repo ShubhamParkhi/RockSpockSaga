637a87adbc7f0738f26226095836adab4f333f1a
Shubham Parkhi <shubhamparkhi10@gmail.com>
2024-01-03 05:41:24 +0530

    Update [contractAddress].tsx

--- pages/play/[contractAddress].tsx
+++ pages/play/[contractAddress].tsx
@@ -1,13 +1,12 @@
-import { NextPage } from "next";
 import { useRouter } from 'next/router';
 import React from "react";
 import WaitingForOpponentsMove from "../../components/Waiting for Opponents Move/WaitingForOpponentsMove";
 
-const WaitingForOpponent: NextPage = () => {
+const Play: React.FC = () => {
   const router = useRouter();
   const { contractAddress } = router.query;
 
   return <WaitingForOpponentsMove contractAddress={contractAddress as string} />;
 };
 
-export default WaitingForOpponent;
+export default Play;
